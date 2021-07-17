f7a16793726358b9ad00fb0f192aff5464e989bd
Dagmar Urbancova <urbancova.dagmar@gmail.com>
2021-07-17 23:11:01 +0300

    modified visuals

--- src/components/GetList.jsx
+++ src/components/GetList.jsx
@@ -13,9 +13,9 @@ userList: [],
 function GetList(props) {
 const { userList } = props;
 return (
-<div className='post-list'>
+<div>
     {userList.map((postDetail, index) => {
-        return <div>
+        return <div className="post">
 {/*         <p>{postDetail.id}</p>  */}
         <h1>{postDetail.name} </h1>
         <p>@{postDetail.username} </p>
